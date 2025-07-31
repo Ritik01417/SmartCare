9a6579541eca8c9c81e0f206445e2a40f3591567
Ritik01417 <ritikkamwal797@gmail.com>
2025-07-31 09:40:18 +0530

    update in basic implimention of internation and doctors card of FeatureSection

--- app/features/doctors/page.jsx
+++ app/features/doctors/page.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 function page() {
   return (
-    <div>
-      <h1>welcome to Expert doctors page</h1>
+    <div style={{color:"aquamarine", height:"300px" , width:"500px",backgroundColor:"whiteSmoke",
+     
+      marginLeft:"500px",
+      marginTop:"100px",
+      borderRadius:"20px",
+      boxShadow: "0 8px 32px rgba(0, 0, 0, 0.7)"
+      
+    }}>
+      <h1 style={{margin:"80px 8px 0 8px",position:"absolute"}}>welcome to Expert doctors page</h1>
     </div>
   )
 }
